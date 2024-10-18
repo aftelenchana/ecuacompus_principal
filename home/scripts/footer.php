5d33892cbef6a6a331474e4f29b97acc8c283424
aftelenchana <alejiss401997@gmail.com>
2024-10-18 02:22:28 -0500

    creacion del footer y reorganizacion de la barra superior

---
+++ home/scripts/footer.php
@@ -0,0 +1,16 @@
+<footer class="content-footer footer bg-footer-theme">
+  <div class="container-xxl">
+    <div
+      class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+      <div class="text-body mb-2 mb-md-0">
+        ©
+        <script>
+          document.write(new Date().getFullYear());
+        </script>
+        , hecho con <span class="text-danger"><i class="tf-icons ri-heart-fill"></i></span> de
+        <a href="https://ecuacompus.com" target="_blank" class="footer-link">ecuacompus</a>
+      </div>
+
+    </div>
+  </div>
+</footer>
